3e7d29dd8b37f595358b346dca79b23c15805924
Ed Hertzog <edhertzog@gmail.com>
2016-06-29 15:10:29 +0000

    Added phpMyAdmin

--- src/php/functions.php
+++ src/php/functions.php
@@ -1,3 +1,22 @@
 <?php
-//functions
+
+/*
+Stub out functions that return true that will eventually do the following things:
+
+- list all books
+- delete a book
+- add a book
+- edit a book
+
+Example stub function that will eventually calculate a total:
+
+function sumTotal ($options) {
+    return true;
+}
+
+*/
+
+
+
+
 ?>
