c3b75ffb2c8e20a4db687e572a9ef775ebe8b574
Stacy Moorhead <spezzola@uarts.edu>
2016-07-20 23:56:13 +0000

    put list.php into template, added nav to template, edited css

--- src/html/head.php
+++ src/html/head.php
@@ -6,4 +6,13 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
+        <header class="container"> 
+            <div class="row">
+                <ul class="nav">
+                    <li><a href="add.php">Add Books</a></li>
+                    <li><a href="edit.php">Edit Books</a></li>
+                    <li><a href="list.php">Book List</a></li>
+                </ul>
+            </div>   
+        </header>     
         
--- src/html/list.php
+++ src/html/list.php
@@ -10,32 +10,40 @@ $booksKeys = array_keys($books[0]);
 
 //print_r($books);
 
-
 ?>
 
 
-
-    <Table>
-        <tr>
-            <?php 
-                for ($i=0; $i < count($booksKeys); $i++){
-                    echo "<th>" . $booksKeys[$i] . "</th>";
-                } 
-            ?>
-        </tr>
-        
-            <?php
-            for ($i=0; $i < count($books); $i++){
-                $book = $books[$i];
-                echo "<tr>";
-                foreach ($book as $bookPart){
-                    echo "<td>" . $bookPart . "</td>";
+<div class="container">
+    <div class="row">
+        <h1 class="center">Reading List</h1>
+    </div>    
+</div>  
+<div class="container">
+    <div class="row">       
+        <Table>
+            <tr>
+                <?php 
+                    for ($i=0; $i < count($booksKeys); $i++){
+                        echo "<th>" . $booksKeys[$i] . "</th>";
+                    } 
+                ?>
+            </tr>
+            
+                <?php
+                for ($i=0; $i < count($books); $i++){
+                    $book = $books[$i];
+                    echo "<tr>";
+                    foreach ($book as $bookPart){
+                        echo "<td>" . $bookPart . "</td>";
+                    }
+                    echo "</tr>";
                 }
-                echo "</tr>";
-            }
-            ?> 
-        
-    </Table>
+                ?> 
+            
+        </Table>
+    </div>
+</div>
+
 <?php
 require 'foot.php';
 ?>
